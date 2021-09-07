df02b9d380f156a5782449becd6912ca29eb4fc9
Macéo Prévost <maceo.prevost@outlook.fr>
2021-09-07 16:33:10 +0200

    fix arrow patern

--- Assets/Scripts/Maceo/Arrow.cs
+++ Assets/Scripts/Maceo/Arrow.cs
@@ -3,25 +3,34 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
+    [SerializeField] private Transform[] waypoints = new Transform[0];
+
 
     public const string HORIZONTAL_NAME = "Horizontal";
 
+    private int index;
+    private int moveIndex = 1;
     private Vector3 velocity = Vector3.zero;
-
-    //private void Start()
-    //{
-    //}
 
     private void Update()
     {
-        //Debug.Log(Input.GetAxis(HORIZONTAL_NAME));
-        velocity.x += Input.GetAxis(HORIZONTAL_NAME) * speed * Time.deltaTime;
-        velocity.y = 6f;
-
-        velocity.x = Mathf.Clamp(velocity.x, -7f, 7f);
-        transform.position = velocity;
-
+        MoveBetweenWaypoints();
         
-        
+    }
+
+    private void MoveBetweenWaypoints()
+    {
+        Vector3 nextPosition = waypoints[index].position;
+
+        // TO DO -> accelerate the speed with time 
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+
+        if (transform.position == nextPosition)
+        {
+            index += moveIndex;
+
+            if (index == 0 || index == waypoints.Length - 1)
+                moveIndex *= -1;
+        }
     }
 }
