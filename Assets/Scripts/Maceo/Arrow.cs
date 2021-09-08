d46c8759157425e78477eef2b83390b28771700a
Macéo Prévost <maceo.prevost@outlook.fr>
2021-09-08 14:23:50 +0200

    Add star and halo particule

--- Assets/Scripts/Maceo/Arrow.cs
+++ Assets/Scripts/Maceo/Arrow.cs
@@ -8,7 +8,11 @@ public class Arrow : MonoBehaviour
     [Header("AccelertionEvent")]
     [SerializeField] private float eventSpeed = 10f;
     [SerializeField] private float timeOfAcceleration = 3f;
-    [SerializeField] private float timeBetweenEvent = 10f;
+    [SerializeField] private float timeBetweenSpeedEvent = 10f;
+
+    [Header("StopEvent")]
+    [SerializeField] private float timeOfStop = 1f;
+    [SerializeField] private float timeBetweenStopEvent = 25f;
 
     [Space]
     [SerializeField] private Transform[] waypoints = new Transform[0];
@@ -17,7 +21,8 @@ public class Arrow : MonoBehaviour
 
     private int index;
     private int moveIndex = 1;
-    private float elaspedTime;
+    private float speedEventCounter;
+    private float  stopEventCounter;
 
     private Coroutine currentCoroutine = null;
     //private Vector3 velocity = Vector3.zero;
@@ -26,6 +31,7 @@ public class Arrow : MonoBehaviour
     {
         MoveBetweenWaypoints();
         AccelerateEvent();
+        StopEvent();
     }
 
     private void MoveBetweenWaypoints()
@@ -51,8 +57,8 @@ public class Arrow : MonoBehaviour
     private void AccelerateEvent()
     {
         //il faudrait que elapsedTime ne s'incremente que quand la fléche est visible et pas quand on doit choisir
-        elaspedTime += Time.deltaTime;
-        if (elaspedTime >= timeBetweenEvent && currentCoroutine == null)
+        speedEventCounter += Time.deltaTime;
+        if (speedEventCounter >= timeBetweenSpeedEvent && currentCoroutine == null)
         {
             currentCoroutine = StartCoroutine(AccelerationCoroutine());
         }
@@ -66,7 +72,27 @@ public class Arrow : MonoBehaviour
 
         speed = currentSpeed;
         currentCoroutine = null;
-        elaspedTime = 0f;
+        speedEventCounter = 0f;
+    }
 
+
+    private void StopEvent()
+    {
+        stopEventCounter += Time.deltaTime;
+        if (stopEventCounter >= timeBetweenStopEvent && currentCoroutine == null)
+        {
+            currentCoroutine = StartCoroutine(StopCoroutine());
+        }
+    }
+
+    private IEnumerator StopCoroutine()
+    {
+        float currentSpeed = speed;
+        speed = 0f;
+        yield return new WaitForSeconds(timeOfStop);
+
+        speed = currentSpeed;
+        currentCoroutine = null;
+        stopEventCounter = 0f;
     }
 }
